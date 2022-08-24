1bf6ddbc7d093ece41c1766ec8e7f5247e3536e7
Cecilie <cecilierd98@gmail.com>
2022-08-24 16:25:25 +0200

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+  <div>
+    <h1 className={'text-3xl text-blue-500'}>
+      Hello World
+    </h1>
+  </div>
+  )
+}
+
+export default Home
